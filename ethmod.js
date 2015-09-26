506d8cf2702fa19508a2c10135ec10abc1f53890
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2015-09-25 20:42:12 -0400

    Update ethmod.js
    Added rep notification

--- ethmod.js
+++ ethmod.js
@@ -1,7 +1,7 @@
 function checkAllBalances() { 
 var i =0; 
 eth.accounts.forEach( function(e){
-    console.log("  eth.accounts["+i+"]: " +  e + " \tbalance: " + web3.fromWei(eth.getBalance(e), "ether") + " ether"); 
+    console.log("  eth.accounts["+i+"]: " +  e + " \tbalance: " + web3.fromWei(eth.getBalance(e), "ether") + " ether & " + token.coinBalanceOf.call(eth.accounts[i])/100 + "% of Tech Enterprises REP"); 
 i++; 
 })
 }; 
